de4914bb3ed9eb9b27f9325641dcf6c642cff726
Andrew <asundquistdavis@gmail.com>
2023-08-20 11:02:45 -0500

    v2

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React, { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./components/App";
-import 'bootstrap';
-import '/src/styles.css'
-import '/src/styles.scss'; 
-
-const root = createRoot(document.getElementById('root'));
-root.render(<StrictMode><App/></StrictMode>);
