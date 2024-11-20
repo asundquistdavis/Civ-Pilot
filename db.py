11ac50ee59ae1fa8a846231649de309455b5d7e2
Andrew <asundquistdavis@gmail.com>
2024-11-20 09:42:32 -0600

    new psql db endpoint

--- db.py
+++ db.py
@@ -29,8 +29,6 @@ local_url = 'sqlite:///db.sqlite'
 protical, user, password, host, port, database = (os.environ['protical'], os.environ['user'], os.environ['password'], os.environ['host'], os.environ['port'], os.environ['database']) if production else (config.protical, config.user, config.password, config.host, config.port, config.database)
 production_url = f'{protical}://{user}:{password}@{host}:{port}/{database}'
 url = local_url if os.environ.get('prod') else local_url
-url = production_url
-print(url)
 engine = create_engine(url)
 
 print(url)
