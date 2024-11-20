d774339daac6fbdcfc022f56a6a771a63ec4c48c
Andrew <asundquistdavis@gmail.com>
2024-11-20 09:45:37 -0600

    new psql db endpoint

--- db.py
+++ db.py
@@ -706,12 +706,11 @@ class Game(Base):
     
     def __repr__(self) -> str: return f'{self.host}\'s game'
 
-if __name__ == '__main__':
-    Base.metadata.create_all(engine)
-    with Session(engine) as session:
-        with open('assets/tradecards.json', 'r') as trade_file:
-            trade_cards = load(trade_file)['tradeCards']
-            TradeCard.create_all(session, trade_cards)
-        with open('assets/advancements.json', 'r') as adv_file:
-            advancement_cards = load(adv_file)['advancements']
-            AdvCard.create_all(session, advancement_cards)
+Base.metadata.create_all(engine)
+with Session(engine) as session:
+    with open('assets/tradecards.json', 'r') as trade_file:
+        trade_cards = load(trade_file)['tradeCards']
+        TradeCard.create_all(session, trade_cards)
+    with open('assets/advancements.json', 'r') as adv_file:
+        advancement_cards = load(adv_file)['advancements']
+        AdvCard.create_all(session, advancement_cards)
